ff6d6904a57fd4eafee0cb989b5a09448c952d02
Gabriel <gabrielfigueira98@gmail.com>
2021-04-30 19:40:36 -0400

    new version

--- Lab1/subtraction.c
+++
@@ -1,6 +0,0 @@
-#include "subtraction.h"
-
-int subtraction(int a, int b){
-    int result = a - b;
-    return result;
-}
--- Lab1/subtraction.h
+++
@@ -1,6 +0,0 @@
-#ifndef SUBTRACTION_H
-#define SUBTRACTION_H
-
-int subtraction(int a, int b);
-
-#endif
