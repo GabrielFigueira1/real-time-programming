436c57e4bb4eb122bc01ede800d1cd4f49d6f29f
Gabriel <gabrielfigueira98@gmail.com>
2021-05-01 17:26:07 -0400

    added comments

--- Lab2/include/dstring.h
+++ Lab2/include/dstring.h
@@ -6,36 +6,23 @@
 struct dstring;
 typedef struct dstring *dstring;
 
+/* Retorna uma nova string a partir de str */
 dstring str_new(char *str);
-
+/* Retorna uma copia de str */
 dstring str_copy(dstring str);
-
+/* Retorna uma nova string resultado da concatenacao
+de str1 e str2 */
 dstring str_concat(dstring str1, dstring str2);
-
+/* Metodo destrutor */
 void str_free(dstring str);
-
-int str_lenght (dstring str);
-
+/* Retorna o numero de caracteres em str*/
+int str_lenght(dstring str);
+/* Retorna uma string no formato char*
+Pode ser usado pelo macro $(str) */
 char *str_buffer(dstring str);
-
+/* Imprime uma string no terminal*/
 void str_print(dstring str);
-
+/* Retorna o tamanho de uma string no formato char* */
 int cstr_len(char *str);
-
-/* 
-
-A manipulação de strings deve conter, ao menos:
-– Definição (tipo) da ADT (dstring).
-– Criação de dstrings a partir de um char*.
-– Criação de dstrings a partir de tipos primitivos (char, int, long,
-float etc.)
-– Criação de dstrings a partir de outras dstrings
-– Concatenação de dstrings
-– Retorno do tamanho da dstrings
-– Retorno do buffer char*
-– Criar um #define $ para esta função de retorno do buffer (na prática
-criando-se um operador para tranformar um dstring em um char*)
-
-*/
 
 #endif
--- Lab2/src/dstring.c
+++ Lab2/src/dstring.c
@@ -10,7 +10,7 @@ struct dstring
 
 dstring str_new(char *str)
 {
-    if(str == NULL)
+    if (str == NULL)
         return NULL;
 
     //Construtor
@@ -19,12 +19,12 @@ dstring str_new(char *str)
     new->size = cstr_len(str);
 
     //Aloca memoria para a string dentro do struct
-    new->str = malloc(sizeof(char)*new->size);
-    //Escreve a string dentro do novo struct
-    for(int i = 0; i<new->size; i++)
+    new->str = malloc(sizeof(char) * new->size);
+    //Escreve a string dentro da dstring
+    for (int i = 0; i < new->size; i++)
     {
         new->str[i] = str[i];
-    }    
+    }
 
     return new;
 }
@@ -38,14 +38,17 @@ dstring str_copy(dstring str)
 
 dstring str_concat(dstring str1, dstring str2)
 {
+    // Tamanho da nova string
     int size = str1->size + str2->size;
+    // Copia str1 em new
     dstring new = str_copy(str1);
-    new->str = realloc(new->str, sizeof(char)*size);
-
+    // Aumenta o espaco alocado para new
+    new->str = realloc(new->str, sizeof(char) * size);
     new->size = size;
-    for(int i = str1->size; i<size; i++)
+    // Escreve os dados de str2 em new
+    for (int i = str1->size; i < size; i++)
     {
-        new->str[i] = str2->str[i-(str1->size)];
+        new->str[i] = str2->str[i - (str1->size)];
     }
 
     return new;
@@ -58,13 +61,13 @@ void str_free(dstring str)
 
 int str_lenght(dstring str)
 {
-    return str->size;    
+    return str->size;
 }
 
 char *str_buffer(dstring str)
 {
-    char *buf = malloc(sizeof(char)*str->size);
-    for(int i = 0; i<str->size; i++)
+    char *buf = malloc(sizeof(char) * str->size);
+    for (int i = 0; i < str->size; i++)
     {
         buf[i] = str->str[i];
     }
@@ -73,10 +76,10 @@ char *str_buffer(dstring str)
 
 void str_print(dstring str)
 {
-    for(int i = 0; i<str->size; i++)
+    for (int i = 0; i < str->size; i++)
     {
         printf("%c", str->str[i]);
-    }    
+    }
     printf("\n");
 }
 
@@ -85,7 +88,8 @@ int cstr_len(char *str)
     //Encontra o tamanho da string
     //O ultimo elemento da string eh sempre '\0'
     int size;
-    for(size = 0; str[size] != '\0'; size++);
+    for (size = 0; str[size] != '\0'; size++)
+        ;
 
     return size;
 }
