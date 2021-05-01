c71943eb3401cc2cf5482c74fb49aeeb4604b0fe
Gabriel <gabrielfigueira98@gmail.com>
2021-05-01 17:55:33 -0400

    test programn included

--- Lab2/src/main.c
+++ Lab2/src/main.c
@@ -2,16 +2,33 @@
 #include "dstring.h"
 
 int main()
-{
-    char *a = "03"; //10
-    char *b = " O2"; //10
+{   
+    // Criacao de novas strings
+    dstring str1 = str_new("Esta eh uma string.");
+    dstring str2 = str_new("Aqui estah outra string!");
+    
+    str_print(str1);
+    str_print(str2);
 
-    dstring n = str_new(a);
-    dstring m = str_new(b);
-    dstring o = str_concat(m, n);
-    b = $(o);
+    // Copia de strings
+    str1 = str_copy(str2);
+    printf("\nString copiada:\n");
+    str_print(str1);
 
-    for(int i = 0; i < 5; i++){
-        printf("%c", b[i]);
-    }
+    str_free(str1);
+    str_free(str2);
+    // Concatenacao
+    dstring str3 = str_new("Hello ");
+    dstring str4 = str_new("World!");
+
+    dstring str5 = str_concat(str3, str4);
+    printf("\nString concatenada:\n");
+    str_print(str5);
+
+    // Tamanho da string
+    printf("Tamanho da string acima: %d\n", str_lenght(str5));
+
+    // Teste do macro $
+    printf("\nTeste do macro:\n");
+    printf("%s\n", $(str5));
 }
