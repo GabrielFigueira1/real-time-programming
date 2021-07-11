8a138749c8d156c78a3ccf8fccdc91b66b42910d
Gabriel <gabrielfigueira98@gmail.com>
2021-07-10 20:12:58 -0400

    Lab8 time measure

--- Lab8/src/syssim.c
+++ Lab8/src/syssim.c
@@ -7,11 +7,11 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-#define BASE_TIME_NS 10e6 //10ms em ns
+#define BASE_TIME_NS 10e6        //10ms em ns
 #define REFERENCE_PERIOD_NS 50e6 //50ms
 #define REFERENCE_PERIOD_S 0.050 //50ms
 
-#define END_TIME_S 4*M_PI
+#define END_TIME_S 13 //13 segundos
 
 double reference_time;
 struct timespec tp;
@@ -35,22 +35,35 @@ double get_time()
     int start = clock_gettime(CLOCK_REALTIME, &tp);
     long start_s = tp.tv_sec;
     long start_ns = tp.tv_nsec;
-    
-    double current_time = ((double)start_s + (double)start_ns * 1e-9) - start_time; 
+
+    double current_time = ((double)start_s + (double)start_ns * 1e-9) - start_time;
 }
 
 double timespec_to_sec(struct timespec *tp)
 {
     long start_s = tp->tv_sec;
     long start_ns = tp->tv_nsec;
-    
-    double current_time = ((double)start_s + (double)start_ns * 1e-9) - start_time; 
+
+    double current_time = ((double)start_s + (double)start_ns * 1e-9) - start_time;
 }
 
 //Configura o tempo de duracao de uma thread no struct timespec
-void set_period(int period, struct timespec *__tp)
+void set_period(double calculated_time, int period, struct timespec *__tp)
 {
+    //Compensa o tempo de processamento para melhor sincronia do processo
+    double new_period = (double)period*1e-9 - (get_time() - calculated_time);
+    new_period *= 1e9;
+    period = (int)new_period;
+
     __tp->tv_nsec += period;
+
+    //Verifica se tv_nsec ultrapassa o limite de 1e9
+    //e faz a correcao dos valores
+    if (__tp->tv_nsec >= 1e9)
+    {
+        __tp->tv_nsec -= 1e9;
+        __tp->tv_sec += 1;
+    }
 }
 
 pthread_mutex_t mtx_reference = PTHREAD_MUTEX_INITIALIZER;
@@ -61,28 +74,37 @@ void run_simulation()
     pthread_mutex_init(&mtx_reference, NULL);
     pthread_mutex_lock(&mtx_reference);
     pthread_t t1, t2, t3, t4, t5;
-    
+
     pthread_create(&t1, NULL, thr_reference, NULL);
     pthread_join(t1, NULL);
 }
 
-
 void *thr_reference()
-{   
+{
     int k = 0;
-    while ((k*REFERENCE_PERIOD_S) <= END_TIME_S){
-    double start = get_time();
-    printf("inicio: %.9lf\n", start);
-    
-    clock_gettime(CLOCK_REALTIME, &timeout_ref);
-    set_period(REFERENCE_PERIOD_NS, &timeout_ref);
-    int try_wait = pthread_mutex_timedlock(&mtx_reference, &timeout_ref);
+    while ((k * REFERENCE_PERIOD_S) <= END_TIME_S)
+    {
+        double calculated_time = k * REFERENCE_PERIOD_S;
 
-    double end = get_time();
-    printf("fim: %.9lf\n", end);
-    double comp_time = timespec_to_sec(&timeout_ref) - start;
-    printf("Tempo de computação ns: %lf\n", comp_time);
-    k++;
+        //escreve o valor de tempo medido
+        //colocar mutex
+        reference_time = get_time();
+
+        clock_gettime(CLOCK_REALTIME, &timeout_ref);
+        set_period(calculated_time ,REFERENCE_PERIOD_NS, &timeout_ref);
+
+        //espera
+        int try_wait = pthread_mutex_timedlock(&mtx_reference, &timeout_ref);
+
+        /*
+        //testes
+        printf("inicio: %.9lf\n", reference_time);
+        double end = get_time();
+        printf("fim: %.9lf\n", end);
+        double comp_time = end - reference_time;
+        printf("Tempo de computação ns: %lf\n", comp_time);
+        printf("k= %d\n\n", k);
+        */
+        k++;
     }
 }
-
